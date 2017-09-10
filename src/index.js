c09b8779d07d9091b3b644e4f10c707790c2e628
Jakub Benes <jukben@gmail.com>
2017-09-10 18:40:53 +0200

    feature: added name field. The name is also first index of keywords array.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import sort from 'match-sorter';
 /* build proper library with included name of the emoji */
 const library = Object.entries(emojilib.lib).map(([name, emojiObject]) => ({
   ...emojiObject,
-  keywords: [...emojiObject.keywords, name],
+  name: name,
+  keywords: [name, ...emojiObject.keywords],
 }));
 
 module.exports = key => sort(library, key, { keys: ['keywords'] });

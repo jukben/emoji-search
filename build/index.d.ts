bafc5594ad4359b1ecd840a11ae4313040657567
Jakub Benes <jukben@gmail.com>
2019-07-23 17:26:54 +0200

    chore: ignore build/ folder

--- build/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare const _default: (key: string) => {
-    keywords: string[];
-    name: string;
-    char: string;
-    fitzpatrick_scale: boolean;
-    category: string;
-}[];
-export default _default;
